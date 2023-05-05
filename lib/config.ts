88a1180699e22da264e4859317966f7e9f0f65db
Willin Wang <willin@willin.org>
2023-05-05 11:48:42 +0800

    fix: rm test api token

--- lib/config.ts
+++ lib/config.ts
@@ -2,4 +2,4 @@ export const BaseURL = process.env.BASE_URL || 'http://localhost:3000';
 // email, uid, username
 export const AdminType = process.env.ADMIN_TYPE || 'username';
 export const AdminId = process.env.ADMIN_ID || 'willin';
-export const ApiToken = process.env.API_TOKEN || 'hello';
+export const ApiToken = process.env.API_TOKEN || '';
